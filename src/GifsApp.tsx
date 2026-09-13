9c8dfc4fbcb1ccd5d487c2665a25e9955ee61cb9
Moilab <moilab.web@gmail.com>
2026-09-13 16:15:55 +0200

    feat(): previousSearch state

--- src/GifsApp.tsx
+++ src/GifsApp.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { GifList } from "./gifs/components/GifList";
 import { PreviousSearches } from "./gifs/components/PreviousSearches";
 import { mockGifs } from "./mock-data/gifs.mock";
@@ -5,6 +6,12 @@ import { CustomHeader } from "./shared/components/CustomHeader";
 import { SearchBar } from "./shared/components/SearchBar";
 
 export const GifsApp = () => {
+  const [previousTerms, setPreviousTerms] = useState<string[]>(["Goku"]);
+
+  const handleTermClicked = (term: string) => {
+    console.log(term);
+  };
+
   return (
     <>
       {/* Header */}
@@ -17,7 +24,10 @@ export const GifsApp = () => {
       <SearchBar placeholder="Buscar gifs" />
 
       {/* Búsquedas previas */}
-      <PreviousSearches searches={["gatos", "perros", "animales"]} />
+      <PreviousSearches
+        searches={previousTerms}
+        onTermClicked={handleTermClicked}
+      />
 
       {/* Gifs */}
       <GifList gifs={mockGifs} />
--- src/gifs/components/PreviousSearches.tsx
+++ src/gifs/components/PreviousSearches.tsx
@@ -1,14 +1,22 @@
 interface PreviousSearchesProps {
   searches: string[];
+  onTermClicked: (term: string) => void;
 }
 
-export const PreviousSearches = ({ searches }: PreviousSearchesProps) => {
+export const PreviousSearches = ({
+  searches,
+  onTermClicked,
+}: PreviousSearchesProps) => {
   return (
     <div className="previous-searches">
       <h2>Búsquedas previas</h2>
       <ul className="previous-searches-list">
         {searches.map((search) => {
-          return <li key={search}>{search}</li>;
+          return (
+            <li key={search} onClick={() => onTermClicked(search)}>
+              {search}
+            </li>
+          );
         })}
       </ul>
     </div>
