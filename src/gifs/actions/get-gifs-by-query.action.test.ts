352bf13989012d4fa2886633aed7ede3d20f46ce
Moilab <moilab.web@gmail.com>
2026-09-17 21:32:34 +0200

    feat(): get gifs actions test

--- src/gifs/actions/get-gifs-by-query.action.test.ts
+++ src/gifs/actions/get-gifs-by-query.action.test.ts
@@ -1,4 +1,4 @@
-import { describe, test, expect } from "vitest";
+import { describe, test, expect, beforeEach } from "vitest";
 import { getGifsByQueryAction } from "./get-gifs-by-query.action";
 import AxiosMockAdapter from "axios-mock-adapter";
 import { giphyApi } from "../api/giphy.api";
@@ -6,6 +6,10 @@ import { giphyDataMock } from "../../../tests/mocks/giphy.data";
 
 describe("getGifsByQueryAction", () => {
   const mock = new AxiosMockAdapter(giphyApi);
+
+  beforeEach(() => {
+    mock.reset();
+  });
 
   test("should return a list of gifs", async () => {
     mock.onGet("/search").reply(200, giphyDataMock);
@@ -23,5 +27,23 @@ describe("getGifsByQueryAction", () => {
         height: expect.any(Number),
       });
     });
+  });
+
+  test("should return an empty list of gifs if query is not provided", async () => {
+    mock.onGet("/search").reply(200, { data: [] });
+
+    const gifs = await getGifsByQueryAction("");
+
+    expect(gifs.length).toBe(0);
+  });
+
+  test("should handle error when the API returns an error", async () => {
+    mock
+      .onGet("/search")
+      .reply(500, { data: { message: "Internal server error" } });
+
+    const gifs = await getGifsByQueryAction("Naruto");
+
+    expect(gifs.length).toBe(0);
   });
 });
--- src/gifs/actions/get-gifs-by-query.action.ts
+++ src/gifs/actions/get-gifs-by-query.action.ts
@@ -3,20 +3,27 @@ import type { Gif } from "../interfaces/gif.interface";
 import { giphyApi } from "../api/giphy.api";
 
 export const getGifsByQueryAction = async (query: string): Promise<Gif[]> => {
-  const response = await giphyApi.get<GiphyResponse>("/search", {
-    params: {
-      q: query,
-      limit: 10,
-    },
-  });
+  if (query.trim().length === 0) return [];
 
-  const gifs = response.data.data.map((gif) => ({
-    id: gif.id,
-    title: gif.title,
-    url: gif.images.original.url,
-    width: Number(gif.images.original.width),
-    height: Number(gif.images.original.height),
-  }));
+  try {
+    const response = await giphyApi.get<GiphyResponse>("/search", {
+      params: {
+        q: query,
+        limit: 10,
+      },
+    });
 
-  return gifs;
+    const gifs = response.data.data.map((gif) => ({
+      id: gif.id,
+      title: gif.title,
+      url: gif.images.original.url,
+      width: Number(gif.images.original.width),
+      height: Number(gif.images.original.height),
+    }));
+
+    return gifs;
+  } catch (error) {
+    console.error("Error fetching gifs by query: ", error);
+    return [];
+  }
 };
